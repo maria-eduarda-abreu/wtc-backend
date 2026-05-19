43bf6808a22f819466db51fceaada9949707f93c
Maria Abreu@Eduarda <mariaeduardaabreu005@gmail.com>
2026-05-18 21:06:37 -0300

    add dependencys spring web, secury,mongodb, auth, firebase

---
+++ src/main/java/br/com/fiap/wtc_backend/WtcBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wtc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/wtc_backend/WtcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wtc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
